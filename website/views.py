b8ec868f830830dbe4c518bd36068d7f2c7551ac
Sebastian Velasco <sesbas778@gmail.com>
2022-10-16 09:37:01 -0500

    se agregan nuevas lineas

--- website/views.py
+++ website/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def welcome(request):
     return HttpResponse("welcome to Meting Planner!")
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
